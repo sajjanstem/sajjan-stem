734f91df18bb86ee43fc6c34da1e93b9b0e2434d
sajjanstem <sajjanstem@gmail.com>
2024-07-07 16:27:29 +0530

    YUVA STEM about & contact

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import "./AboutUs.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const AboutUs = () => {
     return (
-        <div className="section p-4">
+        <div className="section">
             <Helmet>
                 <title>About Us - Yuva STEM Organisation</title>
                 <meta name="description" content="Learn more about Yuva STEM Organisation, dedicated to nurturing young minds through the STEM Genius Hunt Examination." />
@@ -11,16 +13,32 @@ const AboutUs = () => {
                 <meta name="author" content="Yuva STEM Organisation" />
             </Helmet>
             <h2>About Us</h2>
-            <p>
-                At Yuva STEM Organisation, founded by Karthik Sajjan, an alumnus of the Indian Institute of Information Technology Dharwad, we are dedicated to nurturing the intellectual growth and curiosity of young minds through our STEM Genius Hunt Examination. This initiative aims to inspire a deep understanding and passion for STEM subjects among students from classes 6 to 10, fostering critical thinking, problem-solving skills, and innovative knowledge application.
-            </p>
-
-            <p>Aligned with the National Education Policy 2020, our exams emphasize essential skills such as digital literacy, coding, and artificial intelligence. By integrating these into our curriculum, we prepare students for success in an evolving technological landscape. The STEM Genius Hunt Examinations ignite a lifelong passion for STEM, providing a platform for young minds to showcase their talents and fostering innovation and creativity.
-            </p>
-
-            <p>
-                Our mission is to inspire and educate the next generation of STEM leaders. By encouraging curiosity and analytical thinking. The STEM Genius Hunt Examinations prepare students for global challenges and future success. We invite educational institutions to join us in this exciting journey to cultivate a lasting passion for STEM.
-            </p>
+            <div className="content-container">
+                <div className="profile-card">
+                    <img src={process.env.PUBLIC_URL + "/assets/karthik.png"} alt="Karthik Sajjan" className="profile-photo" />
+                    <div className="profile-details">
+                        <h3>Karthik Sajjan</h3>
+                        <p className="position">Founder</p>
+                        <p className="description">
+                            Karthik Sajjan, an alumnus of the Indian Institute of Information Technology (IIIT) Dharwad, holds Olympiad Rank 1 in Level 2 of the International Olympiad of Mathematics 2015 conducted by SilverZone, and secured a gold batch in Mathematica-2011.
+                        </p>
+                        <a href="https://in.linkedin.com/in/karthik-sajjan-3949691a6" target="_blank" rel="noopener noreferrer">
+                            <LinkedInIcon />
+                        </a>
+                    </div>
+                </div>
+                <div className="about-content">
+                    <p>
+                        Yuva STEM Organisation is dedicated to nurturing the intellectual growth and curiosity of young minds through our STEM Genius Hunt Examination. This initiative aims to inspire a deep understanding and passion for STEM subjects among students from classes 6 to 10, fostering critical thinking, problem-solving skills, and innovative knowledge application.
+                    </p>
+                    <p>
+                        Aligned with the National Education Policy 2020, our exams emphasize essential skills such as digital literacy, coding, and artificial intelligence. By integrating these into our curriculum, we prepare students for success in an evolving technological landscape. The STEM Genius Hunt Examinations ignite a lifelong passion for STEM, providing a platform for young minds to showcase their talents and fostering innovation and creativity.
+                    </p>
+                    <p>
+                        Our mission is to inspire and educate the next generation of STEM leaders by encouraging curiosity and analytical thinking. The STEM Genius Hunt Examinations prepare students for global challenges and future success. We invite educational institutions to join us in this exciting journey to cultivate a lasting passion for STEM.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -4,34 +4,32 @@ import "./ContactUs.css";
 
 const ContactUs = () => {
     return (
-        <div className="section">
+        <div className="contact-section p-4">
             <Helmet>
                 <title>Contact Us - Yuva STEM Organisation</title>
                 <meta name="description" content="Contact Yuva STEM Organisation for more information, sample papers, exam fees, and to register for our Genius Hunt exams." />
                 <meta name="keywords" content="Yuva STEM Organisation, contact, STEM education, Genius Hunt exams, registration, Karthik Sajjan, Yuva STEM" />
                 <meta name="author" content="Yuva STEM Organisation" />
             </Helmet>
-            <h2>Contact Us</h2>
+            <div className="contact-header">
+                <h2>Contact</h2>
+            </div>
             <div className="contact-container">
                 <div className="contact-info">
-                    <h3>Email & Phone</h3>
-                    <p>7975176756</p>
+                    <h3>Get in touch</h3>
+                    <h4>Address</h4>
+                    <p>Yuva STEM Organisation</p>
+                    <p>131/A, 3rd Floor, NGEF Layout, Outer ring road, Ring Road, near Grand Seasons Business Hotel, East of NGEF Layout, Kasturi Nagar, Bengaluru, Karnataka 560043</p>
+                    <h4>Email</h4>
                     <p><a href="mailto:info@yuvastem.org">info@yuvastem.org</a></p>
                     <p><a href="mailto:karthiksajjan1@gmail.com">karthiksajjan1@gmail.com</a></p>
-                    <h3>Address</h3>
-                    <address>
-                        Yuva STEM Organisation<br />
-                        131/A, 3rd Floor, NGEF Layout, Outer ring road, Ring Road, near Grand Seasons Business Hotel,<br />
-                        East of NGEF Layout, Kasturi Nagar, Bengaluru, Karnataka 560043
-                    </address>
+                    <h4>Phone</h4>
+                    <p>7975176756</p>
                 </div>
                 <div className="map-container">
                     <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62200.28460990648!2d77.58683584863279!3d13.0026648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae11c26db122c5%3A0xe95ca23faa9830e2!2sYuva%20STEM%20Organisation!5e0!3m2!1sen!2sin!4v1720342114872!5m2!1sen!2sin"
-                        width="600"
-                        height="450"
-                        style={{ border: 0 }}
-                        allowFullScreen
+                        allowFullScreen={true}
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                         title="Yuva STEM Organisation"
